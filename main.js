e7c6ec63c1fdce9999cfcb1704ddaa91b534c777
Jordan Schaefer <jschaef99@gmail.com>
2020-03-24 14:19:27 -0700

    More testing with images

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+//Rick and Morty Planets
+
 $.ajax({
   method: "GET",
   url: "https://rickandmortyapi.com/api/location/",
   success: result
 })
+
+//Rick and Morty Characters
 
 $.ajax({
   mathod: "GET",
@@ -10,17 +14,30 @@ $.ajax({
   success: result
 })
 
+// Rick and Morty Specific Character
+
 $.ajax({
   method: "GET",
-  url: "https://api.spacexdata.com/v3/capsules/C112",
+  url: "https://rickandmortyapi.com/api/character/2",
+  success: image,
+  error: errorMsg
+})
+
+// SpaceX Capsules
+
+$.ajax({
+  method: "GET",
+  url: "https://api.spacexdata.com/v3/capsules",
   success: result
 })
 
 $.ajax({
   method: "GET",
-  url: "https://api.spacexdata.com/v3/capsules",
+  url: "https://api.spacexdata.com/v3/ships",
   success: resultX
 })
+
+// SpaceX Status
 
 $.ajax({
   method: "GET",
@@ -39,4 +56,16 @@ function resultX(data) {
 
 function resultXStatus(data) {
   console.log(data[1].status);
+}
+
+function image(data){
+  var body = document.querySelector("body");
+  var img = document.createElement("img");
+  var newImage = data.image;
+  img.setAttribute("src", newImage);
+  body.appendChild(img);
+}
+
+function errorMsg(error){
+  console.error(error);
 }
