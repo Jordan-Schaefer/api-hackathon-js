d435c31a6ae7b279b9462223cec5a3cab8d66441
Jordan Schaefer <jschaef99@gmail.com>
2020-03-24 15:57:42 -0700

    characters displayed on screen with styling using js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $.ajax({
 $.ajax({
   mathod: "GET",
   url: "https://rickandmortyapi.com/api/character/",
-  success: result
+  success: image
 })
 
 // Rick and Morty Specific Character
@@ -58,17 +58,49 @@ function resultXStatus(data) {
   console.log(data[1].status);
 }
 
-// function image(data) {
-//   var body = document.querySelector("body");
-//   for (var i = 0; i < data.results.length; i++) {
+function image(data) {
+  var body = document.querySelector("body");
+  for (var i = 0; i < data.results.length; i++) {
+    var container = document.createElement("div");
+    var innerContainer = document.createElement("div");
+    var h1 = document.createElement("h1");
+    var img = document.createElement("img");
+    var ul = document.createElement("ul");
+    var liStatus = document.createElement("li");
+    var liSpecies = document.createElement("li");
+    var liLocation = document.createElement("li");
+    var button = document.createElement("button");
 
-//     var img = document.createElement("img");
-//     var result = data.results[i].image;
+    h1.textContent = data.results[i].name;
+    var imgResult = data.results[i].image;
+    liStatus.textContent = "Status: " + data.results[i].status;
+    liSpecies.textContent = "Species: " + data.results[i].species;
+    liLocation.textContent = "Location: " + data.results[i].location.name;
+    button.textContent = "Select";
 
-//     img.setAttribute("src", result);
-//     body.appendChild(img);
-//   }
-// }
+    container.setAttribute("class", "container");
+    innerContainer.setAttribute("class", "inner-container");
+    h1.setAttribute("class", "title");
+    img.setAttribute("src", imgResult);
+    img.setAttribute("class", "image");
+    liStatus.setAttribute("class", "list");
+    liSpecies.setAttribute("class", "list");
+    liLocation.setAttribute("class", "list");
+    button.setAttribute("class", "btn");
+
+    innerContainer.appendChild(h1);
+    innerContainer.appendChild(img);
+    innerContainer.appendChild(img);
+    ul.appendChild(liStatus);
+    ul.appendChild(liSpecies);
+    ul.appendChild(liLocation);
+    innerContainer.appendChild(ul);
+    innerContainer.appendChild(button);
+    container.appendChild(innerContainer);
+
+    body.appendChild(container);
+  }
+}
 
 function errorMsg(error) {
   console.error(error);
