39c22a7a206362c19ca4b440479564fc13b991bc
Jordan Schaefer <jschaef99@gmail.com>
2020-03-25 12:48:25 -0700

    spaceX infor and function coming along

--- main.js
+++ main.js
@@ -30,14 +30,7 @@ $.ajax({
 $.ajax({
   method: "GET",
   url: "https://api.spacexdata.com/v3/capsules",
-  success: resultX,
-  error: errorMsg
-})
-
-$.ajax({
-  method: "GET",
-  url: "https://api.spacexdata.com/v3/ships",
-  success: resultX,
+  success: spaceXdata,
   error: errorMsg
 })
 
@@ -76,10 +69,15 @@ var rickAndMorthyPlanets = [
 ];
 
 
+function errorMsg(error) {
+  console.error(error);
+}
+
+
 function rickAndMortyPlanetImage(data) {
   var body = document.querySelector("body");
   var container = document.createElement("div");
-  container.setAttribute("class", "container hidden");
+  container.setAttribute("class", "container");
 
   for (var i = 0; i < data.results.length; i++) {
     var innerContainer = document.createElement("div");
@@ -122,11 +120,64 @@ function rickAndMortyPlanetImage(data) {
 
 
 
+var spaceXShips = [
+  "images/cap101.jpg",
+  "images/cap102.jpg",
+  "images/cap103.jpg",
+  "images/cap104.png",
+  "images/cap105.jpg",
+  "images/cap106.jpg",
+  "images/cap107.jpg",
+  "images/cap108.jpg",
+  "images/cap109.jpg",
+  "images/cap110.jpg",
+  "images/cap111.jpg",
+  "images/cap112.jpg",
+  "images/cap113.jpg",
+  "images/cap201.jpg",
+  "images/cap205.jpg",
+  "images/cap202.jpg",
+  "images/cap203.jpg",
+  "images/cap204.jpg",
+  "images/cap206.jpg"
+];
 
 
-function errorMsg(error) {
-  console.error(error);
+function spaceXCapsules(data){
+  var body = document.querySelector("body");
+  var container = document.createElement("div");
+  container.setAttribute("class", "container");
+  for(var i = 0; i < data.length; i++){
+    var innerContainer = document.createElement("div");
+    var h1 = document.createElement("h1");
+    var image = document.createElement("img");
+    var ul = document.createElement("ul");
+    var liCapsule = document.createElement("li");
+    var liStatus = document.createElement("li");
+    var liMissions = document.createElement("li");
+    var button = document.createElement("button");
+
+    h1.textContent = data[i].capsule_series;
+    var imageResult = spaceXShips[i];
+    liCapsule.textContent = "Capsule: " + data[i].capsule_id;
+    liStatus.textContent = "Status: " + data[i].status;
+    liMissions.textContent = "Missions: " + data[i].missions.length;
+
+    container.setAttribute("class", "container");
+    innerContainer.setAttribute("class", "inner-container");
+    h1.setAttribute("class", "title");
+    img.setAttribute("src", imgResult);
+    img.setAttribute("class", "image");
+    liStatus.setAttribute("class", "list");
+    liSpecies.setAttribute("class", "list");
+    liLocation.setAttribute("class", "list");
+    button.setAttribute("class", "btn");
+
+
+
+  }
 }
+
 
 
 
@@ -134,17 +185,18 @@ function result(data) {
   console.log(data);
 }
 
-function resultX(data) {
+function spaceXdata(data) {
   console.log(data);
 }
 
 function resultXStatus(data) {
-  console.log(data[1].status);
+  console.log(data);
 }
 
 function rickAndMortyCharacterImage(data) {
   var body = document.querySelector("body");
   var container = document.createElement("div");
+
   for (var i = 0; i < data.results.length; i++) {
 
     var innerContainer = document.createElement("div");
